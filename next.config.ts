7b8c6ade7af5e733c6c3e9535a7f016c904780fb
ObakeFilter <edward.romanenco@smarsh.com>
2025-04-01 16:23:16 +0300

    Recreating base NextJs app.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    assetPrefix: '/obakefilter.github.io/',
+    basePath: '/obakefilter.github.io',
+    output: 'export'
 };
 
 export default nextConfig;
