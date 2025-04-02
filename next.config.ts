19c17567db7bb902946ca0d3bccb0612b9b36e9f
ObakeFilter <edward.romanenco@smarsh.com>
2025-04-02 14:09:55 +0300

    Moving to pages router.

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    assetPrefix: '/text-transpose/',
+    basePath: '/obakefilter.github.io',
+    output: 'export'
+};
+
+export default nextConfig;
